1464e8e9b8fee20446f2d995fd562f5ba2461357
FYATMM <fengyi.jump.out@gmail.com>
2019-05-23 18:10:54 +0800

    界面元素：trackbar滑动条，progressbar进度条

--- 俄罗斯方块Tetris/Form1.Designer.cs
+++ 俄罗斯方块Tetris/Form1.Designer.cs
@@ -28,60 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStripMain = new System.Windows.Forms.MenuStrip();
-            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.重新开始ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.replayExtendedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStripMain.SuspendLayout();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.replayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.reviewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemLine = new System.Windows.Forms.ToolStripSeparator();
+            this.exiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.styleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.style1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.style2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.style3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.keyboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBarReview = new System.Windows.Forms.ProgressBar();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.trackBarReviewSpeed = new System.Windows.Forms.TrackBar();
+            this.buttonReview = new System.Windows.Forms.Button();
+            this.buttonReplay = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarReviewSpeed)).BeginInit();
             this.SuspendLayout();
             // 
-            // menuStripMain
+            // menuStrip1
             // 
-            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.文件ToolStripMenuItem});
-            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
-            this.menuStripMain.Name = "menuStripMain";
-            this.menuStripMain.Size = new System.Drawing.Size(260, 25);
-            this.menuStripMain.TabIndex = 6;
-            this.menuStripMain.Text = "menuStrip1";
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(260, 25);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // 文件ToolStripMenuItem
+            // fileToolStripMenuItem
             // 
-            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.重新开始ToolStripMenuItem,
-            this.replayExtendedToolStripMenuItem,
-            this.saveToolStripMenuItem,
-            this.loadToolStripMenuItem});
-            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
-            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
-            this.文件ToolStripMenuItem.Text = "文件";
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.replayToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.reviewToolStripMenuItem,
+            this.toolStripMenuItemLine,
+            this.exiToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.fileToolStripMenuItem.Text = "&文件";
             // 
-            // 重新开始ToolStripMenuItem
+            // replayToolStripMenuItem
             // 
-            this.重新开始ToolStripMenuItem.Name = "重新开始ToolStripMenuItem";
-            this.重新开始ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.重新开始ToolStripMenuItem.Text = "重新开始";
+            this.replayToolStripMenuItem.Name = "replayToolStripMenuItem";
+            this.replayToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.replayToolStripMenuItem.Text = "重新&开始";
             // 
-            // replayExtendedToolStripMenuItem
+            // toolStripMenuItem1
             // 
-            this.replayExtendedToolStripMenuItem.Name = "replayExtendedToolStripMenuItem";
-            this.replayExtendedToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.replayExtendedToolStripMenuItem.Text = "Replay(Extended)";
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "Replay(Extended)";
             // 
-            // saveToolStripMenuItem
+            // toolStripMenuItem2
             // 
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.saveToolStripMenuItem.Text = "Save";
-            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Text = "&Save";
             // 
-            // loadToolStripMenuItem
+            // toolStripMenuItem3
             // 
-            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
-            this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.loadToolStripMenuItem.Text = "Load";
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem3.Text = "&Load";
+            // 
+            // reviewToolStripMenuItem
+            // 
+            this.reviewToolStripMenuItem.Name = "reviewToolStripMenuItem";
+            this.reviewToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.reviewToolStripMenuItem.Text = "Re&view";
+            // 
+            // toolStripMenuItemLine
+            // 
+            this.toolStripMenuItemLine.Name = "toolStripMenuItemLine";
+            this.toolStripMenuItemLine.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exiToolStripMenuItem
+            // 
+            this.exiToolStripMenuItem.Name = "exiToolStripMenuItem";
+            this.exiToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exiToolStripMenuItem.Text = "E&xit";
+            // 
+            // optionToolStripMenuItem
+            // 
+            this.optionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.styleToolStripMenuItem});
+            this.optionToolStripMenuItem.Name = "optionToolStripMenuItem";
+            this.optionToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.optionToolStripMenuItem.Text = "&设置选项";
+            // 
+            // styleToolStripMenuItem
+            // 
+            this.styleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.style1ToolStripMenuItem,
+            this.style2ToolStripMenuItem,
+            this.style3ToolStripMenuItem});
+            this.styleToolStripMenuItem.Name = "styleToolStripMenuItem";
+            this.styleToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.styleToolStripMenuItem.Text = "&Style";
+            // 
+            // style1ToolStripMenuItem
+            // 
+            this.style1ToolStripMenuItem.Name = "style1ToolStripMenuItem";
+            this.style1ToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.style1ToolStripMenuItem.Tag = "";
+            this.style1ToolStripMenuItem.Text = "style1";
+            // 
+            // style2ToolStripMenuItem
+            // 
+            this.style2ToolStripMenuItem.Name = "style2ToolStripMenuItem";
+            this.style2ToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.style2ToolStripMenuItem.Text = "style2";
+            // 
+            // style3ToolStripMenuItem
+            // 
+            this.style3ToolStripMenuItem.Name = "style3ToolStripMenuItem";
+            this.style3ToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.style3ToolStripMenuItem.Text = "style3";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.keyboardToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.helpToolStripMenuItem.Text = "&游戏帮助";
+            // 
+            // keyboardToolStripMenuItem
+            // 
+            this.keyboardToolStripMenuItem.Name = "keyboardToolStripMenuItem";
+            this.keyboardToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.keyboardToolStripMenuItem.Text = "&操作键";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "&关于";
+            // 
+            // progressBarReview
+            // 
+            this.progressBarReview.Location = new System.Drawing.Point(4, 380);
+            this.progressBarReview.Name = "progressBarReview";
+            this.progressBarReview.Size = new System.Drawing.Size(252, 17);
+            this.progressBarReview.TabIndex = 13;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(181, 266);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 12;
+            this.buttonLoad.Text = "载入";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(181, 237);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 11;
+            this.buttonSave.Text = "保存";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // trackBarReviewSpeed
+            // 
+            this.trackBarReviewSpeed.Location = new System.Drawing.Point(181, 291);
+            this.trackBarReviewSpeed.Maximum = 15;
+            this.trackBarReviewSpeed.Minimum = 1;
+            this.trackBarReviewSpeed.Name = "trackBarReviewSpeed";
+            this.trackBarReviewSpeed.Size = new System.Drawing.Size(75, 45);
+            this.trackBarReviewSpeed.TabIndex = 10;
+            this.trackBarReviewSpeed.Value = 1;
+            // 
+            // buttonReview
+            // 
+            this.buttonReview.Location = new System.Drawing.Point(181, 337);
+            this.buttonReview.Name = "buttonReview";
+            this.buttonReview.Size = new System.Drawing.Size(75, 23);
+            this.buttonReview.TabIndex = 9;
+            this.buttonReview.Text = "Re&view";
+            this.buttonReview.UseVisualStyleBackColor = true;
+            // 
+            // buttonReplay
+            // 
+            this.buttonReplay.Location = new System.Drawing.Point(181, 196);
+            this.buttonReplay.Name = "buttonReplay";
+            this.buttonReplay.Size = new System.Drawing.Size(75, 23);
+            this.buttonReplay.TabIndex = 8;
+            this.buttonReplay.Text = "重新开始";
+            this.buttonReplay.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -89,13 +239,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(260, 410);
-            this.Controls.Add(this.menuStripMain);
+            this.Controls.Add(this.progressBarReview);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.trackBarReviewSpeed);
+            this.Controls.Add(this.buttonReview);
+            this.Controls.Add(this.buttonReplay);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MainMenuStrip = this.menuStripMain;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.menuStripMain.ResumeLayout(false);
-            this.menuStripMain.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarReviewSpeed)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,12 +259,29 @@
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStripMain;
-        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 重新开始ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem replayExtendedToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem replayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem reviewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItemLine;
+        private System.Windows.Forms.ToolStripMenuItem exiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem styleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem style1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem style2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem style3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem keyboardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBarReview;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TrackBar trackBarReviewSpeed;
+        private System.Windows.Forms.Button buttonReview;
+        private System.Windows.Forms.Button buttonReplay;
     }
 }
 
