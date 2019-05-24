525c943f00a25a5aefe98b2bc7bd1ac7e86bb674
FYATMM <fengyi.jump.out@gmail.com>
2019-05-24 09:28:08 +0800

    拷贝控件，并分成3个独立控件

---
+++ 俄罗斯方块Tetris/View/youxiNext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 俄罗斯方块Tetris
+{
+    public partial class youxiNext : Control
+    {
+        /// <summary>
+        /// 显示下一个出现的方块控件
+        /// </summary>
+
+        #region 字段
+        private Bitmap backBitmap;
+        #endregion
+
+        #region 构造函数
+        public youxiNext()
+        {
+            //InitializeComponent();
+            BackColor = Color.Black;
+            base.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+        #endregion
+
+        #region 方法
+        public void Clear()
+        {
+            Graphics vGraphics = Graphics.FromImage(backBitmap);
+            vGraphics.FillRectangle(new SolidBrush(BackColor), vGraphics.ClipBounds);
+        }
+
+        public void Update(youxiControl AyouxiControl)
+        {
+            if (AyouxiControl == null) return;
+            Clear();
+            AyouxiControl.DrawNext(Graphics.FromImage(backBitmap));
+            Invalidate();
+        }
+
+        public void SetSize(int AWidth, int AHeight)
+        {
+            Width = AWidth;
+            Height = AHeight;
+            backBitmap = new Bitmap(AWidth, AHeight);
+        }
+
+        #endregion
+
+        #region 事件
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            if (backBitmap != null) pe.Graphics.DrawImage(backBitmap, 0, 0);
+        }
+        #endregion
+    }
+}
---
+++ 俄罗斯方块Tetris/View/youxiScore.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 俄罗斯方块Tetris
+{
+    /// <summary>
+    /// 显示积分信息的控件
+    /// </summary>
+    public partial class youxiScore : Control
+    {
+        #region 字段
+        ////自定义背景图
+        private Bitmap backBitmap;
+        #endregion
+
+        #region 构造函数
+        //创建组件后，自动生成，构造函数
+        public youxiScore()
+        {
+            //InitializeComponent();//自动生成的构造函数定义的方法
+            ////自定义背景色，风格
+            BackColor = Color.Black;
+            base.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+        }
+        #endregion
+
+        #region 方法
+        /// <summary>
+        /// 清空
+        /// </summary>
+        public void Clear()
+        {
+            Graphics vGraphics = Graphics.FromImage(backBitmap);
+            vGraphics.FillRectangle(new SolidBrush(BackColor), vGraphics.ClipBounds);
+        }
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="youxiScore"></param>
+        public void Update(youxiScore youxiScore)
+        {
+            if (youxiScore == null) return;
+            Clear();
+            youxiScore.DrawScore(Graphics.FromImage(backBitmap));
+            Invalidate();
+        }
+
+        /// <summary>
+        /// 设置尺寸
+        /// </summary>
+        /// <param name="AWidth"></param>
+        /// <param name="AHeight"></param>
+        public void SetSize(int AWidth, int AHeight)
+        {
+            Width = AWidth;
+            Height = AHeight;
+            backBitmap = new Bitmap(AWidth, AHeight);
+        }
+
+        /// <summary>
+        /// 绘制积分框
+        /// </summary>
+        /// <param name="AGraphics"></param>
+        public void DrawScore(Graphics AGraphics)
+        {
+            if (AGraphics == null) return;
+            RectangleF vRectangleF = new Rectangle(0, 0, brickWidth * 4, brickHeight);
+
+            StringFormat vStringFormat = new StringFormat();
+            vStringFormat.FormatFlags |= StringFormatFlags.LineLimit;
+            vStringFormat.Alignment = StringAlignment.Center;
+
+            AGraphics.DrawString("Score", new Font(Font, FontStyle.Bold), Brushes.White, vRectangleF, vStringFormat);
+
+            vRectangleF.Offset(0, brickHeight);
+
+            AGraphics.DrawString(score.ToString(), Font, Brushes.White, vRectangleF, vStringFormat);
+
+            vRectangleF.Offset(0, brickHeight);
+
+            AGraphics.DrawString("Level", new Font(Font, FontStyle.Bold),Brushes.White, vRectangleF, vStringFormat);
+
+            vRectangleF.Offset(0, brickHeight);
+
+            AGraphics.DrawString(level.ToString(), Font,Brushes.White, vRectangleF, vStringFormat);
+
+            vRectangleF.Offset(0, brickHeight);
+
+            AGraphics.DrawString("Lines", new Font(Font, FontStyle.Bold), Brushes.White, vRectangleF, vStringFormat);
+
+            vRectangleF.Offset(0, brickHeight);
+
+            AGraphics.DrawString(lines.ToString(), Font,Brushes.White, vRectangleF, vStringFormat);
+        }
+
+        #endregion
+
+        #region 事件
+        //创建组件后，自动生成，用户调用画图
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            //Raises the Paint event.
+            //
+            //Raising an event invokes the event handler through a delegate. 
+            //The OnPaint method also enables derived classes to handle the event without attaching a delegate. 
+            //This is the preferred technique for handling the event in a derived class.
+            //
+            //When overriding OnPaint(PaintEventArgs) in a derived class, be sure to call the base class's OnPaint(PaintEventArgs) method 
+            //so that registered delegates receive the event.
+            base.OnPaint(pe);//自动生成的用户调用画图
+
+            ////画用户自定义图形,DrawImage有30多个重载，这个是DrawImage(Image, Int32, Int32)
+            //Draws the specified image, using its original physical size, at the location specified by a coordinate pair.
+            if (backBitmap != null) pe.Graphics.DrawImage(backBitmap, 0, 0);
+        }
+        #endregion
+    }
+}
